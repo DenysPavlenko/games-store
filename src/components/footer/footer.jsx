26bc72b4e8c5f09ba0290f1d4e885230ec008265
Denys Pavlenko <de.pavlenko@gmail.com>
2020-10-27 14:57:30 +0200

    footer was refactored

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+// Laytou
+import Container from 'layout/container/container';
+import Row from 'layout/row/row';
+import Col from 'layout/col/col';
 // Components
-import Container from 'layout/container/container'
 import SocialIcons from 'components/social-icons/social-icons';
 import Typography from 'components/typography/typography';
 // Styles
@@ -25,17 +28,17 @@ class Footer extends React.Component {
     return (
       <div className="footer">
         <Container>
-          <div className="footer-wrap">
-            <div className="footer-top">
+          <Row>
+            <Col col="12" className="d-flex justify-content-between align-items-center mb-4">
               <SocialIcons socials={socials} />
               <div className="footer-up" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
                 <Chevron />
               </div>
-            </div>
-            <div className="footer-content">
+            </Col>
+            <Col md={12} lg={8} xl={7}>
               <Typography component="p">© {new Date().getFullYear()}, Epic Games, Inc. All rights reserved. Epic, Epic Games, the Epic Games logo, Fortnite, the Fortnite logo, Unreal, Unreal Engine, the Unreal Engine logo, Unreal Tournament, and the Unreal Tournament logo are trademarks or registered trademarks of Epic Games, Inc. in the United States of America and elsewhere. Other brands or product names are the trademarks of their respective owners. Non-US transactions through Epic Games International, S.à r.l.</Typography>
-            </div>
-          </div>
+            </Col>
+          </Row>
         </Container>
       </div>
     );
