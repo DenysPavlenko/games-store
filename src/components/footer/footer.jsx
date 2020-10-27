cfeeacff4d53e5c9e2086182bb9615b2ca77bb82
Denys Pavlenko <de.pavlenko@gmail.com>
2020-10-27 14:50:05 +0200

    responsive styles were added to footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -25,15 +25,15 @@ class Footer extends React.Component {
     return (
       <div className="footer">
         <Container>
-          <div className="footer-content">
-            <div className="footer-content-left">
-              <SocialIcons socials={socials} className="mb-4" />
-              <Typography component="p">© {new Date().getFullYear()}, Epic Games, Inc. All rights reserved. Epic, Epic Games, the Epic Games logo, Fortnite, the Fortnite logo, Unreal, Unreal Engine, the Unreal Engine logo, Unreal Tournament, and the Unreal Tournament logo are trademarks or registered trademarks of Epic Games, Inc. in the United States of America and elsewhere. Other brands or product names are the trademarks of their respective owners. Non-US transactions through Epic Games International, S.à r.l.</Typography>
-            </div>
-            <div className="footer-content-right">
+          <div className="footer-wrap">
+            <div className="footer-top">
+              <SocialIcons socials={socials} />
               <div className="footer-up" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
                 <Chevron />
               </div>
+            </div>
+            <div className="footer-content">
+              <Typography component="p">© {new Date().getFullYear()}, Epic Games, Inc. All rights reserved. Epic, Epic Games, the Epic Games logo, Fortnite, the Fortnite logo, Unreal, Unreal Engine, the Unreal Engine logo, Unreal Tournament, and the Unreal Tournament logo are trademarks or registered trademarks of Epic Games, Inc. in the United States of America and elsewhere. Other brands or product names are the trademarks of their respective owners. Non-US transactions through Epic Games International, S.à r.l.</Typography>
             </div>
           </div>
         </Container>
