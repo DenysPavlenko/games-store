ea0a9fc511ca7583bf2edd9fb9cdd6986831bec3
Denys Pavlenko <de.pavlenko@gmail.com>
2020-10-27 19:01:21 +0200

    scroll-to-top component warning was fixed

--- src/components/scroll-to-top/scroll-to-top.jsx
+++ src/components/scroll-to-top/scroll-to-top.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-class ScrollToTop extends Component {
+class ScrollToTop extends React.Component {
 
   static propTypes = {
     children: PropTypes.node.isRequired,
