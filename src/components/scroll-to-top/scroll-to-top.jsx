242222bedc3fc826b955c08d0d1f9b593f9aa02a
Denys Pavlenko <de.pavlenko@gmail.com>
2021-05-07 13:47:25 +0300

    Implement scroll-to-top tests

--- src/components/scroll-to-top/scroll-to-top.jsx
+++ src/components/scroll-to-top/scroll-to-top.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-class ScrollToTop extends React.Component {
+export class ScrollToTop extends React.Component {
 
   static propTypes = {
     children: PropTypes.node.isRequired,
-  }
+  };
 
   componentDidUpdate(prevProps) {
+    /* istanbul ignore else */
     if (this.props.location.pathname !== prevProps.location.pathname) {
       window.scrollTo(0, 0);
     }
-  }
+  };
 
   render() {
     return this.props.children;
---
+++ src/components/scroll-to-top/scroll-to-top.spec.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { ScrollToTop } from './scroll-to-top';
+import { checkProps } from 'test-utils/index';
+
+const props = {
+  children: <span></span>,
+  location: { pathname: '/test' }
+};
+
+const setup = (props = {}) => {
+  return mount(<ScrollToTop {...props} />)
+};
+
+describe('PropTypes', () => {
+  test('renders without errors', () => {
+    const wrapper = setup({ ...props });
+    expect(wrapper).not.toBeNull();
+  });
+  test('scrolls to top on page change', () => {
+    const mockScrollTo = jest.fn();
+    window.scrollTo = mockScrollTo;
+    const wrapper = setup({ ...props });
+    wrapper.instance().componentDidUpdate({ location: { pathname: '/test2' }});
+    expect(mockScrollTo.mock.calls.length).toBe(1);
+  });
+
+  test('does not throw warning with expected props', () => {
+    const expectedProps = { ...props };
+    const propsError = checkProps(ScrollToTop, expectedProps);
+    expect(propsError).toBeUndefined();
+  });
+});
