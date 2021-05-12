f225c65c9da4000d6a52f863d23f4ce60c7a38cc
Denys Pavlenko <de.pavlenko@gmail.com>
2021-05-12 13:03:53 +0300

    Add placeholder prop to input component

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 // Styles
 import './input.sass';
 
-const Input = ({ className, invalid, isDark, type, name, value, onChange }) => {
+const Input = ({ className, invalid, isDark, type, name, value, placeholder, onChange }) => {
   const classes = classNames({
     input: true,
     'input-invalid': invalid,
@@ -13,7 +13,7 @@ const Input = ({ className, invalid, isDark, type, name, value, onChange }) => {
   });
 
   return (
-    <input className={classes} type={type} name={name} value={value} onChange={onChange} />
+    <input className={classes} type={type} name={name} value={value} placeholder={placeholder} onChange={onChange} />
   );
 };
 
