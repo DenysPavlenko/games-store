723921faf1e4a7070e6456eafce2a459f18efb6d
Denys Pavlenko <de.pavlenko@gmail.com>
2021-07-08 19:30:01 +0300

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,9 @@ module.exports = {
     'jsx-a11y/no-static-element-interactions': 0,
     'jsx-a11y/click-events-have-key-events': 0,
     'react/jsx-one-expression-per-line': 0,
+    'react/state-in-constructor': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'import/no-named-as-default': 0,
   },
   settings: {
     'import/resolver': {
