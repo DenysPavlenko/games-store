a7bbf38ed49b7431f59937c0e011176c3ebfef35
Denys Pavlenko <de.pavlenko@gmail.com>
2021-07-08 19:36:40 +0300

    Update cards tests

--- src/components/cards/cards.spec.js
+++ src/components/cards/cards.spec.js
@@ -1,37 +1,40 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { shallow } from 'enzyme';
-import Cards from './cards';
 import { checkProps } from 'test-utils/index';
+import Cards from './cards';
 
-const props = {
-  children: <span></span>,
+const defProps = {
+  children: <span />,
   isLoading: false,
   hasError: false,
   placeholdersToShow: 2,
 };
 
-const setup = (props = {}) => {
-  return shallow(<Cards {...props} />)
-};
+const setup = (props = {}) => shallow(<Cards {...props} />);
 
 describe('Cards', () => {
   test('renders without errors', () => {
-    const wrapper = setup({ ...props });
+    const wrapper = setup({ ...defProps });
     const component = wrapper.find('.cards');
     expect(component.length).toBe(1);
   });
   test('renders <ErrorIndicator/> on error', () => {
-    const wrapper = setup({ ...props, hasError: true });
+    const wrapper = setup({ ...defProps, hasError: true });
     const component = wrapper.find('ErrorIndicator');
     expect(component.length).toBe(1);
   });
   test('renders <CardPlaceholder/> on loading', () => {
-    const wrapper = setup({ ...props, isLoading: true, placeholdersToShow: 2 });
+    const wrapper = setup({
+      ...defProps,
+      isLoading: true,
+      placeholdersToShow: 2,
+    });
     const component = wrapper.find('CardPlaceholder');
     expect(component.length).toBe(2);
   });
   test('does not throw warning with expected props', () => {
-    const expectedProps = { ...props };
+    const expectedProps = { ...defProps };
     const propsError = checkProps(Cards, expectedProps);
     expect(propsError).toBeUndefined();
   });
